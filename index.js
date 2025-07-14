50649d43e915422ef4edbe94f3237f89f70e84ce
Cheikh Bassirou GUEYE <bagueye905@gmail.com>
2025-07-14 08:46:47 +0000

    use of the product model to store data - p1

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
+//to be allowed to pass json data to node js it's for security concerns
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
-
+    res.send('hello')
 })
 
-app.post('/api/products', (req, res))
+app.post('/api/products', (req, res) => {
+    // Getting data sent in the body of the request and displaying it
+    console.log(req.body)
+
+    // Notification
+    res.send(req.body)
+})
 
 const port = process.env.PORT || 3000
 
