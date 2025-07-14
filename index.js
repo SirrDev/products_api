371c2ba088964321491d72c765b8baf9c83ccb12
Cheikh Bassirou GUEYE <bagueye905@gmail.com>
2025-07-14 08:32:54 +0000

    creation of the product model

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
+
+app.get('/', (req, res) => {
+
+})
+
+app.post('/api/products', (req, res))
+
 const port = process.env.PORT || 3000
 
-mongoose.connect("mongodb+srv://@productsdb.r0qweb5.mongodb.net/Node-API?retryWrites=true&w=majority&appName=ProductsDB")
+mongoose.connect("mongodb+srv://sirrdev:admin905SirrDev-_&3480@productsdb.r0qweb5.mongodb.net/Node-API?retryWrites=true&w=majority&appName=ProductsDB")
 .then(() => {
     console.log('Connected to the database!')
     app.listen(port, () => console.log(`Listenin on port ${port}...`))
---
+++ models/product.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = mongoose.Schema({
+
+    name: {
+        type: String,
+        required: [true, "Please enter product name"]
+    },
+
+    quantity: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+
+    price: {
+        typre: Number,
+        required: true,
+        default: 0
+    },
+
+    image: {
+        type: String,
+        required: false
+    },
+    
+
+},
+    {
+        timestamps: true
+    }
+)
+
+// Allowing mongobd to use the schema above ps the model here is Product
+const Product = mongoose.model("Product", ProductSchema)
+
+//exporting the model in order to use it in other modules to save datas
+module.exports = Product
